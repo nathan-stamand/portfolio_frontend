4d26a3e2cd352f0ae73f4b93f29a4780a0825ddf
nathan-stamand <nathansaint810@gmail.com>
2020-09-02 23:23:38 -0500

    Add background picture, containing summary container, which contains two <p> tags with my name and job title

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div id="background-pic">
+        <div id="summary-container">
+          <p>Nathan St.Amand</p>
+          <p>Full-Stack Web Developer</p>
+        </div>
+      </div>
     </div>
   );
 }
