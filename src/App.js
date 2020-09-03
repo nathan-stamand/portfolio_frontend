f063afe08f4019e4f1c63a2de6ffd3cd1da7f2a2
nathan-stamand <nathansaint810@gmail.com>
2020-09-03 18:57:31 -0500

    Change img#nathan-looking-great source to ./nathan_smiling.png

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           <p>Full-Stack Web Developer</p>
         </div>
         <div id="picture-of-nathan">
-          <img id="nathan-looking-great" alt="Me, looking great no doubt." src={require("./nathan_smiling.jpg")} />
+          <img id="nathan-looking-great" alt="Me, looking great no doubt." src={require("./nathan_smiling.png")} />
         </div>
       </div>
     </div>
