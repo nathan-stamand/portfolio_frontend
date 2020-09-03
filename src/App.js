ae1c2ade4f2e584198dee7882a64a5187f182536
nathan-stamand <nathansaint810@gmail.com>
2020-09-03 11:14:08 -0500

    Add div#picture-of-nathan and contained img#nathan-looking-great inside of div#background-pic

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
           <p>Nathan St.Amand</p>
           <p>Full-Stack Web Developer</p>
         </div>
+        <div id="picture-of-nathan">
+          <img id="nathan-looking-great" alt="Me, looking great no doubt." src={require("./nathan_smiling.jpg")} />
+        </div>
       </div>
     </div>
   );
