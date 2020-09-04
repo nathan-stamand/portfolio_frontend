cd504597cee2784953af729eddedf276c4651840
nathan-stamand <nathansaint810@gmail.com>
2020-09-04 10:08:54 -0500

    wrapped Blog and Social Btns in a div#main-btn-container-b and changed original wrapper to div#main-btn-container-a

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <div id="main-btn-container">
+      <div id="main-btn-container-a">
         <div id="about-btn" class="main btn">ABOUT</div>
         <div id="projects-btn" class="main btn">PROJECTS</div>
+      </div>
+      <div id="main-btn-container-b">
         <div id="blog-btn" class="main btn">BLOG</div>
         <div id="social-btn" class="main btn">SOCIAL</div>
       </div>
