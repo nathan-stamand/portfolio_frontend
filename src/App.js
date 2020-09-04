7445d69317f43e8f7a0b200e0f8b8166028f5ad3
nathan-stamand <nathansaint810@gmail.com>
2020-09-04 15:52:42 -0500

    Change instances of 'btn' to c'card' for clarity

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <div id="main-btn-container-a">
-        <div id="about-btn" className="main btn">
-          <section className="btn-text">ABOUT</section>
+      <div id="main-card-container-a">
+        <div id="about-card" className="main card">
+          <section className="card-text">ABOUT</section>
         </div>
-        <div id="projects-btn" className="main btn">
-          <section className="btn-text">PROJECTS</section>
+        <div id="projects-card" className="main card">
+          <section className="card-text">PROJECTS</section>
         </div>
       </div>
-      <div id="main-btn-container-b">
-        <div id="blog-btn" className="main btn">
-          <section className="btn-text">BLOG</section>
+      <div id="main-card-container-b">
+        <div id="blog-card" className="main card">
+          <section className="card-text">BLOG</section>
         </div>
-        <div id="social-btn" className="main btn">
-          <section className="btn-text">SOCIAL</section>
+        <div id="social-card" className="main card">
+          <section className="card-text">SOCIAL</section>
         </div>
       </div>
     </div>
