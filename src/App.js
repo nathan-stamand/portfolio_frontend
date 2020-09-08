b817a511f48987161121cbe2d02c0c939543085e
nathan-stamand <nathansaint810@gmail.com>
2020-09-08 10:26:03 -0500

    Move input submit button into #section#message-text-submit

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ function App() {
             <label htmlFor="email">Email Address</label>
             <input id="email" type="text" className="email-form"/>
           </section>
-          <section id="message-text">
+          <section id="message-text-submit">
             <label htmlFor="message">Message</label>
             <textarea id="message" className="email-form"/>
+            <input type="submit" value="-submit-" />
           </section>
-          <input type="submit" value="-submit-" />
         </form>
       </div>
     </div>
