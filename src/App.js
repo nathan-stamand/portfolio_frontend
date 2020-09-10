8840cf12bd5eeb5c095fec82d9d3d3c92c87b6bb
nathan-stamand <nathansaint810@gmail.com>
2020-09-10 12:57:30 -0500

    Delete div#contact-form and its contents, add Switch with Routes for about, projects, blog, social, and homepage.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React from 'react';
+import { Route, Switch } from "react-router-dom";
 import './App.css';
 import Header from './Components/Header';
 import MainCards from './Components/MainCards';
+import ContactForm from './Components/ContactForm';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MainCards />
-      <div id="contact-form">
-        <form>
-          <section id="personal-info">
-            <label htmlFor="name">Full Name</label>
-            <input id="name" type="text" className="email-form"/>
-            <label htmlFor="email">Email Address</label>
-            <input id="email" type="text" className="email-form"/>
-          </section>
-          <section id="message-text-submit">
-            <label htmlFor="message">Message</label>
-            <textarea id="message" className="email-form"/>
-            <input type="submit" value="-submit-" />
-          </section>
-        </form>
-      </div>
+        <Switch>
+          <Route path="/about">
+            <p>ABOUT</p>
+          </Route>
+          <Route path="/projects">
+            <p>PROJECTS</p>
+          </Route>
+          <Route path="/blog">
+            <p>BLOG</p>
+          </Route>
+          <Route path="/social">
+            <p>SOCIAL</p>
+          </Route>
+          <Route path="/">
+            <MainCards />
+          </Route>
+        </Switch>
+      <ContactForm />
     </div>
   );
 }
