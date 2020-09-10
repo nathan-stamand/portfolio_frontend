1da6c90f6303490d3b941662acf4030d6485a00b
nathan-stamand <nathansaint810@gmail.com>
2020-09-10 14:05:45 -0500

    Change MainCards from functional to class component, added methods #openBlog and #loadComponent to either open blog in new tab or load the component described by the card when clicked.

--- src/Components/MainCards.js
+++ src/Components/MainCards.js
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-function MainCards() {
-  return (
-    <div id="main-cards-container">
-      <div id="main-card-container-a">
-        <div id="about-card" className="main card">
-          <section className="card-text">ABOUT</section>
+class MainCards extends Component {
+  openBlog = () => {
+    return window.open("https://medium.com/@nathansaint810", "_blank")
+  }
+
+  loadComponent = event => {
+    const keyword = event.target.id.replace('-card', '');
+    this.props.history.push(`/${keyword}`);
+  }
+
+  render() {
+    return (
+      <div id="main-cards-container">
+        <div id="main-card-container-a">
+          <div id="about-card" className="main card" onClick={this.loadComponent}>
+            <section className="card-text" >ABOUT</section>
+          </div>
+          <div id="projects-card" className="main card" onClick={this.loadComponent}>
+            <section className="card-text" >PROJECTS</section>
+          </div>
         </div>
-        <div id="projects-card" className="main card">
-          <section className="card-text">PROJECTS</section>
+        <div id="main-card-container-b">
+          <div id="blog-card" className="main card"onClick={this.openBlog}>
+            <section className="card-text">BLOG</section>
+          </div>
+        <div id="social-card" className="main card" onClick={this.loadComponent}>
+          <section className="card-text">SOCIAL</section>
         </div>
-      </div>
-      <div id="main-card-container-b">
-        <div id="blog-card" className="main card">
-          <section className="card-text">BLOG</section>
-        </div>
-      <div id="social-card" className="main card">
-        <section className="card-text">SOCIAL</section>
       </div>
     </div>
-  </div>
-  )
+    )
+  }
 }
 
-export default MainCards;
+export default withRouter(MainCards);
