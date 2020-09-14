761995cd3308101de45e6b24166383418c8f7738
nathan-stamand <nathansaint810@gmail.com>
2020-09-13 19:37:19 -0500

    Change to class Component, add state for name, email, message and handlers for Changes in inputs/textarea.

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 
-function ContactForm() {
-  return (
-    <div id="contact-form">
-        <form>
-          <section id="personal-info">
-            <label htmlFor="name">Full Name</label>
-            <input id="name" type="text" className="email-form"/>
-            <label htmlFor="email">Email Address</label>
-            <input id="email" type="text" className="email-form"/>
-          </section>
-          <section id="message-text-submit">
-            <label htmlFor="message">Message</label>
-            <textarea id="message" className="email-form"/>
-            <input type="submit" value="-submit-" />
-          </section>
-      </form>
-    </div>
-  )
+class ContactForm extends Component {
+  state = {
+    name: '',
+    email: '',
+    message: '',
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <div id="contact-form">
+          <form>
+            <section id="personal-info">
+              <label htmlFor="name">Full Name</label>
+              <input id="name" type="text" className="email-form" value={this.state.name} onChange={this.handleChange}/>
+              <label htmlFor="email">Email Address</label>
+              <input id="email" type="text" className="email-form" value={this.state.email} onChange={this.handleChange}/>
+            </section>
+            <section id="message-text-submit">
+              <label htmlFor="message">Message</label>
+              <textarea id="message" className="email-form" value={this.state.message} onChange={this.handleChange}/>
+              <input type="submit" value="-submit-" />
+            </section>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default ContactForm;
