a8a2c330ee44d62dbb15ab041fb20e42b5dc4fba
nathan-stamand <nathansaint810@gmail.com>
2020-09-16 13:11:14 -0500

    Remove methods, clear out children of div#contact-form

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,41 +1,10 @@
 import React, { Component } from "react";
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    email: '',
-    message: '',
-  }
-
-  handleChange = event => {
-    this.setState({
-      [event.target.id]: event.target.value
-    })
-  }
-
-  submitHandler = event => {
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <div id="contact-form">
-          <form onSubmit={this.submitHandler}>
-            <section id="personal-info">
-              <label htmlFor="name">Full Name</label>
-              <input id="name" type="text" className="email-form" value={this.state.name} onChange={this.handleChange}/>
-              <label htmlFor="email">Email Address</label>
-              <input id="email" type="text" className="email-form" value={this.state.email} onChange={this.handleChange}/>
-            </section>
-            <section id="message-text-submit">
-              <label htmlFor="message">Message</label>
-              <textarea id="message" className="email-form" value={this.state.message} onChange={this.handleChange}/>
-              <input type="submit" value="-submit-" />
-            </section>
-        </form>
-      </div>
-    )
-  }
+function ContactForm() {
+  return (
+    <div id="contact-form">    
+    </div>
+  )
 }
 
 export default ContactForm;
