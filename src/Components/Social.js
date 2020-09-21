fd518e47564bb6428bf9d36ded438da5de96fd1d
nathan-stamand <nathansaint810@gmail.com>
2020-09-20 19:04:12 -0500

    Add social media icons to Social.js

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function Social() {
   return (
-    <div id="social">  
+    <div id="social">
+      <div className="icon-container">
+        <img src={require('../Github Icon.png')} alt="Github" />
+      </div>
+      <div className="icon-container">
+        <img src={require('../LinkedIn Icon.png')} alt="LinkedIn" />
+      </div>
+      <div className="icon-container">
+        <img src={require('../Twitter Icon.png')} alt="Twitter" />
+      </div>
     </div>
   )
 }
