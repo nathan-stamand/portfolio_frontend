7ba6f166d1315330fd1fa6f90d483a91fdc09137
nathan-stamand <nathansaint810@gmail.com>
2020-09-22 09:21:23 -0500

    Rephrase content

--- src/Components/About.js
+++ src/Components/About.js
@@ -34,21 +34,11 @@ function About() {
       <section id="more-about-me" className="about">
         <h3>Why Would You Hire a Guy Like Me?</h3>
         <p>
-          Fair enough question! From what I've heard, I'm a "nice enough" guy people 
-          "don't hate" working with, for one. Also, I'm typically pretty quiet, but 
-          if someone initiates conversation with me I find myself pretty
-          <span className="bold"> dang</span> social. Not in a weird, intense way -- in a really
-          cool, natural way. In fact, it's weird that I'm talking about it, because it's really
-          seamless. You probably never would have thought about it if I didn't let you know. 
-        </p>
-        <p>
-          On a more serious note, I'm not only a fast learner but someone who prides himself on 
-          learning <span className="italics">well</span>. I enjoy learning new things, I take pride in
-          my work, and I like having systems for myself. I spent over five years being "just an accountant" -- 
-          I stopped my hobbies, I didn't feel creative, and I spent my weekends dreading my jobs. Now, I've 
-          found something that makes me feel accomplished at the end of the day. That's why I'd make a good 
-          employee. I worked hard when I didn't enjoy my job, now I'm ready to see what I can do when I'm excited
-          to work.
+          Fair enough question! I know how to learn new things,
+          and I take pride in my work. I've got a border collie, chihuahua mix, and 
+          rabbit to support with my wife. So I'm a family guy and a team player, in 
+          that sense. I have a wide array of interests, so I like talking to people
+          about their hobbies or side hustles. I dunno -- I think I'm fun.
         </p>
       </section>
     </div>
